ca066331c93c3f9c4095a657717e20a81216c9d6
LarrySon786 <richardsbrandon3@gmail.com>
2025-05-18 21:02:26 -0400

    Completed Assignment

---
+++ scripts/temples.js
@@ -0,0 +1,27 @@
+//Assigning which variables will appear and where they appear on the HTML page
+const year = document.querySelector("#year");
+let lastModified = document.querySelector("#modified")
+
+//Displaying the year and my copyright information
+const today = new Date();
+year.innerHTML = `<span>©${today.getFullYear()} Brandon Richards | Ohio</span>`;
+
+//Displaying the last modified date
+let oLastModified = new Date(document.lastModified);
+lastModified.innerHTML = `<span>Last Modified: ${oLastModified} </span>`;
+
+
+//Menu Set-up
+
+const navigation = document.querySelector(".navigation");
+const hammenu = document.querySelector("#menu");
+
+hammenu.addEventListener('click', () => {
+    navigation.classList.toggle('show');
+    hammenu.classList.toggle('show');
+})
+
+
+
+
+
