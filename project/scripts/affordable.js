6aee47d26217f587af16959c48afb89e2a21d7ff
LarrySon786 <richardsbrandon3@gmail.com>
2025-06-18 21:46:40 -0400

    updates

--- project/scripts/affordable.js
+++ project/scripts/affordable.js
@@ -12,63 +12,66 @@ lastModified.innerHTML = `<span>Last Modified: ${oLastModified} </span>`;
 
 
 //Calculate Average Insurance Cost
-let age = document.querySelector('#insurance-age');
-let cost = document.querySelector('#expected-cost');
-let button = document.querySelector('#insurance-button');
+const age = document.querySelector('#insurance-age');
+const cost = document.querySelector('#expected-cost');
+const button = document.querySelector('#insurance-button');
 
-
-button.addEventListener('click', function () {
-    
-    if (age.value.trim() !== "") {
-        if (age.value >= 18 && age.value <= 25) {
-            cost.value = 445;
-        }
-        else if (age.value > 25 && age.value <= 32) {
-            cost.value = 467;
-        }
-        else if (age.value > 32 && age.value <= 40) {
-            cost.value = 537;
-        }
-        else if (age.value > 40 && age.value <= 50) {
-            cost.value = 682;
-        }
-        else if (age.value > 50 && age.value <= 60) {
-            cost.value = 1001.50;
-        }
-        else if (age.value > 60) {
-            cost.value = 1208;
+if (button) {
+    button.addEventListener('click', function () {
+        
+        if (age.value.trim() !== "") {
+            if (age.value >= 18 && age.value <= 25) {
+                cost.value = 445;
+            }
+            else if (age.value > 25 && age.value <= 32) {
+                cost.value = 467;
+            }
+            else if (age.value > 32 && age.value <= 40) {
+                cost.value = 537;
+            }
+            else if (age.value > 40 && age.value <= 50) {
+                cost.value = 682;
+            }
+            else if (age.value > 50 && age.value <= 60) {
+                cost.value = 1001.50;
+            }
+            else if (age.value > 60) {
+                cost.value = 1208;
+            }
+            else {
+                cost.value = "";
+            }
+            cost.textContent = `On average, the cost of health insurance for someone who is ${age.value} years old is $${cost.value}.`;
+            age.focus();
         }
         else {
             cost.value = "";
+            cost.textContent = "";
         }
-        cost.textContent = `On average, the cost of health insurance for someone who is ${age.value} years old is $${cost.value}.`;
         age.focus();
     }
-    else {
-        cost.value = "";
-        cost.textContent = "";
-    }
-    age.focus();
+    )
 }
-)
-
 
 
 //Record how many different forms they have submitted using local storage
-let formSubmission = document.querySelector('#form-submission')
-let patientName = document.querySelector('#patient-name')
-let patientPhone = document.querySelector('#patient-phone')
-let patientEmail = document.querySelector('#patient-email')
+const formSubmissionButton = document.querySelector('#form-submission');
+const patientName = document.querySelector('#patient-name');
+const patientPhone = document.querySelector('#patient-phone');
+const patientEmail = document.querySelector('#patient-email');
 
-let formsubmissions = getFormSubmissions() || 0;
+let formsBefore = getFormSubmissions() || 0;
 
-formSubmission.addEventListener('submit', setFormSubmissions()
-)
+if (formSubmissionButton) {
+    formSubmissionButton.addEventListener('submit', setFormSubmissions()
+    )
+}
 
 
 
-function setFormSubmissions() {
-    formSubmissions = formSubmissions + 1;
+
+function setFormSubmissions(formsBefore) {
+    formSubmissions = formsBefore + 1;
     localStorage.setItem('formsSubmitted', JSON.stringify(formSubmissions));
     localStorage.setItem('patientName', JSON.stringify(patientName));
     localStorage.setItem('patientPhone', JSON.stringify(patientPhone));
